5cc1f8ac1f255d4691717435afc9a378f3916b95
pedro <pfreitas@id.uff.br>
2020-11-17 16:40:50 -0300

    adding mvc arch

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Pedro Freitas | Zoop on 17/11/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
